2f6c26e8381888d22462e2184aac0a434420edd7
Schubi <schubi@erlangen.ccc.de>
2013-06-17 16:58:25 +0200

    Base: Option to enable Notification Light when screen is on [1/2]
    This makes it possible to let the Notification LED flash even when the screen is on
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/NotificationManagerService.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ibd9b9083d843c1bfe59b450b245d5334c58b7348

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5137,6 +5137,13 @@ public final class Settings {
         public static final String HEADS_UP_EXCLUDE_FROM_LOCK_SCREEN = "heads_up_exclude_from_lock_screen";
 
         /**
+         * Screen-On Notification Light,
+         * should default to 1 (yes, Notification Light is enabled when screen is on)
+         * @hide
+         */
+        public static final String SCREEN_ON_NOTIFICATION_LED = "screen_on_notification_led";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
