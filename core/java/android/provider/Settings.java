0def1f54680c2cbd67c8f5d0e1e6a11e7df2900d
kecinzer <kecinzer@gmail.com>
2014-03-14 07:11:42 +0100

    NFC Polling Mode (3/3)
    Change-Id: I41c76e6f23642af3daf356daf3788387b396594f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8914,6 +8914,14 @@ public final class Settings {
                 "battery_saver_option";
 
         /**
+         * NFC polling mode configuration key
+         *
+         * @hide
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling_mode";
+
+        /**
+         * Whether volume up/down can be long pressed to skip tracks
          * @hide
          */
         public static final String BATTERY_SAVER_NORMAL_MODE =
