5f8a05591baceff1d8b67c817d98660c5ed4e8d8
Sean Hoyt <seanhoyt963@gmail.com>
2014-07-23 13:09:09 -0400

    Revert "AudioManager: Add Bluetooth A2DP profile trigger"
    This reverts commit b9b68e20c555efc3197993cc4e96e17b5e594efa.
    
    Change-Id: I9ee7ef9e2c2c82d55458e7ffb889a3ea209fa1af

--- core/java/android/app/Profile.java
+++ core/java/android/app/Profile.java
@@ -95,8 +95,6 @@ public final class Profile implements Parcelable, Comparable {
         public static final int ON_CONNECT = 0;
         public static final int ON_DISCONNECT = 1;
         public static final int DISABLED = 2;
-        public static final int ON_A2DP_CONNECT = 3;
-        public static final int ON_A2DP_DISCONNECT = 4;
     }
 
     public static class ProfileTrigger implements Parcelable {
@@ -255,7 +253,7 @@ public final class Profile implements Parcelable, Comparable {
     public void setTrigger(int type, String id, int state, String name) {
         if (id == null
                 || type < TriggerType.WIFI || type > TriggerType.BLUETOOTH
-                || state < TriggerState.ON_CONNECT || state > TriggerState.ON_A2DP_DISCONNECT) {
+                || state < TriggerState.ON_CONNECT || state > TriggerState.DISABLED) {
             return;
         }
 
