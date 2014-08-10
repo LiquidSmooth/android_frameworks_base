AICP/frameworks_base

MIUI Style carrier text in the statusbar [1/2]
Change-Id: Ib1d8d62b0e575277a779738b9820156e1a3fc999

packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -104,12 +104,12 @@ public class CarrierText extends TextView {
     }
 
     protected void updateCarrierText(State simState, CharSequence plmn, CharSequence spn) {
-        String customLabel = Settings.System.getString(getContext().getContentResolver(),
-                Settings.System.CUSTOM_CARRIER_LABEL);
-        if (customLabel == null || customLabel.length() == 0) {
-            setText(getCarrierTextForSimState(simState, plmn, spn));
+        String customCarrierLabel = Settings.System.getStringForUser(getContext().getContentResolver(),
+            Settings.System.CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+        if (!TextUtils.isEmpty(customCarrierLabel)) {
+            setText(customCarrierLabel);
         } else {
-            setText(customLabel);
+            setText(getCarrierTextForSimState(simState, plmn, spn));
         }
     }
 

packages/SystemUI/src/com/android/systemui/statusbar/phone/CarrierLabel.java
@@ -1,0 +1,189 @@
+/*
+ * Copyright (C) 2014 The MoKee OpenSource Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.phone;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.aicp.util.AicpUtils;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.internal.telephony.TelephonyIntents;
+
+import com.android.systemui.statusbar.util.SpnOverride;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import com.android.systemui.R;
+
+public class CarrierLabel extends TextView {
+
+    private boolean mAttached;
+
+    private static boolean isCN;
+
+    private Context mContext;
+
+    protected int mCarrierColor = com.android.internal.R.color.white;
+    Handler mHandler;
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateColor();
+        }
+    }
+
+    public CarrierLabel(Context context) {
+        this(context, null);
+    }
+
+    public CarrierLabel(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        updateNetworkName(false, null, false, null);
+        mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+        updateColor();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        if (!mAttached) {
+            mAttached = true;
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(TelephonyIntents.SPN_STRINGS_UPDATED_ACTION);
+            filter.addAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
+            mContext.registerReceiver(mIntentReceiver, filter, null, getHandler());
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mAttached) {
+            mContext.unregisterReceiver(mIntentReceiver);
+            mAttached = false;
+        }
+    }
+
+    private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (TelephonyIntents.SPN_STRINGS_UPDATED_ACTION.equals(action)
+                    || Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED.equals(action)) {
+                updateNetworkName(intent.getBooleanExtra(TelephonyIntents.EXTRA_SHOW_SPN, false),
+                        intent.getStringExtra(TelephonyIntents.EXTRA_SPN),
+                        intent.getBooleanExtra(TelephonyIntents.EXTRA_SHOW_PLMN, false),
+                        intent.getStringExtra(TelephonyIntents.EXTRA_PLMN));
+                isCN = AicpUtils.isChineseLanguage();
+            }
+        }
+    };
+
+    void updateNetworkName(boolean showSpn, String spn, boolean showPlmn, String plmn) {
+        if (false) {
+            Log.d("CarrierLabel", "updateNetworkName showSpn=" + showSpn + " spn=" + spn
+                    + " showPlmn=" + showPlmn + " plmn=" + plmn);
+        }
+        final String str;
+        final boolean plmnValid = showPlmn && !TextUtils.isEmpty(plmn);
+        final boolean spnValid = showSpn && !TextUtils.isEmpty(spn);
+        if (plmnValid && spnValid) {
+            str = plmn + "|" + spn;
+        } else if (plmnValid) {
+            str = plmn;
+        } else if (spnValid) {
+            str = spn;
+        } else {
+            str = "";
+        }
+        String customCarrierLabel = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+        if (!TextUtils.isEmpty(customCarrierLabel)) {
+            setText(customCarrierLabel);
+        } else {
+            setText(TextUtils.isEmpty(str) ? getOperatorName() : str);
+        }
+    }
+
+    private String getOperatorName() {
+        String operatorName = getContext().getString(R.string.quick_settings_wifi_no_network);
+        TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(
+                Context.TELEPHONY_SERVICE);
+        if (isCN) {
+            String operator = telephonyManager.getNetworkOperator();
+            if (TextUtils.isEmpty(operator)) {
+                operator = telephonyManager.getSimOperator();
+            }
+            SpnOverride mSpnOverride = new SpnOverride();
+            operatorName = mSpnOverride.getSpn(operator);
+            if (TextUtils.isEmpty(operatorName)) {
+                operatorName = telephonyManager.getSimOperatorName();
+            }
+        } else {
+            operatorName = telephonyManager.getNetworkOperatorName();
+            if (TextUtils.isEmpty(operatorName)) {
+                operatorName = telephonyManager.getSimOperatorName();
+            }
+        }
+        return operatorName.toUpperCase();
+    }
+
+    private void updateColor() {
+        int newColor = 0;
+        mCarrierColor = Settings.System.getInt(mContext.getContentResolver(),
+                            Settings.System.STATUS_BAR_CARRIER_COLOR, newColor);
+
+        if  (mCarrierColor == Integer.MIN_VALUE) {
+             // flag to reset the color
+             mCarrierColor = newColor;
+        }
+        setTextColor(mCarrierColor);
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -283,6 +283,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mNotificationShortcutsHideCarrier;
     FrameLayout.LayoutParams lpCarrierLabel;
 
+    // Status bar carrier
+    private boolean mShowStatusBarCarrier;
+
     private View mWeatherHeader;
     private boolean mWeatherEnabled = false;
 
@@ -1719,6 +1722,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if ((diff & StatusBarManager.DISABLE_CLOCK) != 0) {
             boolean show = (state & StatusBarManager.DISABLE_CLOCK) == 0;
             showClock(show);
+            //add CarrierLabel
+            showStatusBarCarrierLabel(show);
         }
         if ((diff & StatusBarManager.DISABLE_EXPAND) != 0) {
             if ((state & StatusBarManager.DISABLE_EXPAND) != 0) {
@@ -3175,6 +3180,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    public void showStatusBarCarrierLabel(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        View statusBarCarrierLabel = mStatusBarView.findViewById(R.id.status_bar_carrier_label);
+        if (statusBarCarrierLabel != null) {
+            statusBarCarrierLabel.setVisibility(show ? (mShowStatusBarCarrier ? View.VISIBLE : View.GONE) : View.GONE);
+        }
+    }
+
     private void resetUserSetupObserver() {
         mContext.getContentResolver().unregisterContentObserver(mUserSetupObserver);
         mUserSetupObserver.onChange(false);
@@ -3683,6 +3697,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.HEADS_UP_NOTIFCATION_DECAY),
                     false, this, UserHandle.USER_ALL);
 
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CARRIER), false, this);
+
             updateSettings();
 
         }
@@ -3805,6 +3822,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0, UserHandle.USER_CURRENT) == 1;
         systemColor = Settings.System.getIntForUser(cr,
                 Settings.System.SYSTEM_ICON_COLOR, -2, UserHandle.USER_CURRENT);
+
+        mShowStatusBarCarrier = Settings.System.getInt(
+            cr, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
+        showStatusBarCarrierLabel(mShowStatusBarCarrier);
 
         updateBatteryIcons();
         updateCustomHeaderStatus();

LiquidSmooth/android_frameworks_base

packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -95,10 +95,10 @@ public class CarrierText extends TextView {
     }
 
     protected void updateCarrierText(State simState, CharSequence plmn, CharSequence spn) {
-        String customLabel = Settings.System.getString(mContext.getContentResolver(),
-                Settings.System.NOTIFICATION_CUSTOM_CARRIER_LABEL);
-        if (customLabel != null && customLabel.length() > 0) {
-            setText(customLabel);
+        String customCarrierLabel = Settings.System.getStringForUser(getContext().getContentResolver(),
+                Settings.System.NOTIFICATION_CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+        if (!TextUtils.isEmpty(customCarrierLabel)) {
+            setText(customCarrierLabel);
         } else {
             setText(getCarrierTextForSimState(simState, plmn, spn));
         }

packages/SystemUI/src/com/android/systemui/statusbar/phone/CarrierLabel.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2014 The MoKee OpenSource Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.phone;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.internal.util.liquid.LiquidUtils;
+import com.android.internal.telephony.TelephonyIntents;
+import com.android.systemui.statusbar.util.SpnOverride;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import com.android.systemui.R;
+
+public class CarrierLabel extends TextView {
+
+    private boolean mAttached;
+
+    private static boolean isCN;
+
+    private Context mContext;
+
+    protected int mCarrierColor = com.android.internal.R.color.white;
+    Handler mHandler;
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System
+                    .getUriFor(Settings.System.STATUS_BAR_CARRIER_COLOR), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateColor();
+        }
+    }
+
+    public CarrierLabel(Context context) {
+        this(context, null);
+    }
+
+    public CarrierLabel(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CarrierLabel(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mContext = context;
+        updateNetworkName(false, null, false, null);
+        mHandler = new Handler();
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
+        updateColor();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        if (!mAttached) {
+            mAttached = true;
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(TelephonyIntents.SPN_STRINGS_UPDATED_ACTION);
+            filter.addAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
+            mContext.registerReceiver(mIntentReceiver, filter, null, getHandler());
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mAttached) {
+            mContext.unregisterReceiver(mIntentReceiver);
+            mAttached = false;
+        }
+    }
+
+    private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (TelephonyIntents.SPN_STRINGS_UPDATED_ACTION.equals(action)
+                    || Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED.equals(action)) {
+                updateNetworkName(intent.getBooleanExtra(TelephonyIntents.EXTRA_SHOW_SPN, false),
+                        intent.getStringExtra(TelephonyIntents.EXTRA_SPN),
+                        intent.getBooleanExtra(TelephonyIntents.EXTRA_SHOW_PLMN, false),
+                        intent.getStringExtra(TelephonyIntents.EXTRA_PLMN));
+                isCN = LiquidUtils.isChineseLanguage();
+            }
+        }
+    };
+
+    void updateNetworkName(boolean showSpn, String spn, boolean showPlmn, String plmn) {
+        if (false) {
+            Log.d("CarrierLabel", "updateNetworkName showSpn=" + showSpn + " spn=" + spn
+                    + " showPlmn=" + showPlmn + " plmn=" + plmn);
+        }
+        final String str;
+        final boolean plmnValid = showPlmn && !TextUtils.isEmpty(plmn);
+        final boolean spnValid = showSpn && !TextUtils.isEmpty(spn);
+        if (plmnValid && spnValid) {
+            str = plmn + "|" + spn;
+        } else if (plmnValid) {
+            str = plmn;
+        } else if (spnValid) {
+            str = spn;
+        } else {
+            str = "";
+        }
+        String customCarrierLabel = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.NOTIFICATION_CUSTOM_CARRIER_LABEL, UserHandle.USER_CURRENT);
+        if (!TextUtils.isEmpty(customCarrierLabel)) {
+            setText(customCarrierLabel);
+        } else {
+            setText(TextUtils.isEmpty(str) ? getOperatorName() : str);
+        }
+    }
+
+    private String getOperatorName() {
+        String operatorName = getContext().getString(R.string.quick_settings_wifi_no_network);
+        TelephonyManager telephonyManager = (TelephonyManager) getContext().getSystemService(
+                Context.TELEPHONY_SERVICE);
+        if (isCN) {
+            String operator = telephonyManager.getNetworkOperator();
+            if (TextUtils.isEmpty(operator)) {
+                operator = telephonyManager.getSimOperator();
+            }
+            SpnOverride mSpnOverride = new SpnOverride();
+            operatorName = mSpnOverride.getSpn(operator);
+            if (TextUtils.isEmpty(operatorName)) {
+                operatorName = telephonyManager.getSimOperatorName();
+            }
+        } else {
+            operatorName = telephonyManager.getNetworkOperatorName();
+            if (TextUtils.isEmpty(operatorName)) {
+                operatorName = telephonyManager.getSimOperatorName();
+            }
+        }
+        return operatorName.toUpperCase();
+    }
+
+    private void updateColor() {
+        int newColor = 0;
+        mCarrierColor = Settings.System.getInt(mContext.getContentResolver(),
+                            Settings.System.STATUS_BAR_CARRIER_COLOR, newColor);
+
+        if  (mCarrierColor == Integer.MIN_VALUE) {
+             // flag to reset the color
+             mCarrierColor = newColor;
+        }
+        setTextColor(mCarrierColor);
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -364,6 +364,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mShakeEnabled;
     private boolean mUserPresent;
 
+    // Status bar carrier
+    private boolean mShowStatusBarCarrier;
+
     // drag bar
     private int mCloseViewHeight;
 
@@ -600,6 +603,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_GRAVITY_BOTTOM), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_CARRIER), false, this,
                     UserHandle.USER_ALL);
             update();
         }
@@ -855,6 +861,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 mWeatherEnabled = weatherHolder;
                 enableOrDisableWeather();
             }
+
+            mShowStatusBarCarrier = Settings.System.getInt(
+                    resolver, Settings.System.STATUS_BAR_CARRIER, 0) == 1;
+                    showStatusBarCarrierLabel(mShowStatusBarCarrier);
         }
     }
 
@@ -2556,6 +2566,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if ((diff & StatusBarManager.DISABLE_CLOCK) != 0) {
             boolean show = (state & StatusBarManager.DISABLE_CLOCK) == 0;
             showClock(show);
+            showStatusBarCarrierLabel(show);
         }
         if ((diff & StatusBarManager.DISABLE_EXPAND) != 0) {
             if ((state & StatusBarManager.DISABLE_EXPAND) != 0) {
@@ -4471,6 +4482,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mContext.getResources().getConfiguration()
                             .orientation == Configuration.ORIENTATION_LANDSCAPE
                     && mNavigationBarCanMove);
+        }
+    }
+
+    public void showStatusBarCarrierLabel(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        View statusBarCarrierLabel = mStatusBarView.findViewById(R.id.status_bar_carrier_label);
+        if (statusBarCarrierLabel != null) {
+            statusBarCarrierLabel.setVisibility(show ? (mShowStatusBarCarrier ? View.VISIBLE : View.GONE) : View.GONE);
         }
     }
 