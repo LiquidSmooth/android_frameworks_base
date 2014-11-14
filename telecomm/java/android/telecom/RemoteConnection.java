a376b3771fb324fb5d661079e062149d2fc6688e
DvTonder <david.vantonder@gmail.com>
2014-11-14 09:44:50 -0800

    blacklist: squash (2/3)
    ** Also squash CM telephony commits
    
    Optimize IncallUI delay for voice calls
    
    In current implementation detectCountry method is called from telephony
    as many times as 34 for one MO voice call, causing increasing delay to appear
    incallui for user.  This fix ensures that there is only one call to
    detectCountry method per one voice call thus optimizing delay.
    
    CRs-Fixed: 803069
    frameworks: add blacklist call logging (1/3)
    
    frameworks: fix Blacklist support for L (3/3)
    
    Proper supplementary service notification handling (1/5).
    
    Remove CAF's passing of implementation details from the base classes,
    and replace it by well-defined call properties.
    
    framework: add blacklist disconnect cause
    
    Constant to be used when marking a Call as blacklisted.
    
    Add SMS Middleware layer.
    
    Add fix so ordered broadcasts are delivered to system
    apps first in the event of a tie. This works around
    the issue where terrible apps steal SMS notifications
    from the broadcast queue to simply prevent notifications
    that could otherwise be removed by a toggle.
    
    IMS: Add emergency call failures
    
    Define emergency call failure errors to support
    emergency redial feature for multi sim.
    
    telephony: Add compatibility version of registrationDisconnected()
    
     * Assign ids to all methods and use new AIDL feature to generate
       the correct code.
     * This should ensure compatibility with proprietary IMS
       implementations.
    
    telephony: Add AOSP compatibility methods for IMS
    
    Adds changes from 2cdac3b IMS: change getMasterValue to getProvisionedValue
    but leaves current methods in place to preserve BSP.
    
    ims: More backwards-compatibility stuff
    
     * Let's just add another copy of the AOSP header for IImsConfig, as
       the implementation is different on the proprietary side than what
       is the latest Qualcomm BSP.
    
    ims: Bring in line with current release
    
    Revert "IMS: UT ServiceClass"
    
     * Bring us in line with correct QC release
    
    This reverts commit 65a47e85e57e3a4f22a23f2e35db1aef1e20f8e3.
    
    Revert "IMS: Supplementary services"
    
    This reverts commit f669dc4549ee6aacf9a789ddb4db4b4f93599e40.
    
    Telephony: Modify RILConstants in accordance with LMR1 RIL.h
    
    IMS: add the api to get IMS registration information
    
    Revert "telephony: Add compatibility version of registrationDisconnected()"
    
    telecomm: remove duplicate method
    
    Change-Id: Ife88f9b6521f496585ae502745b963179577b703

--- telecomm/java/android/telecom/RemoteConnection.java
+++ telecomm/java/android/telecom/RemoteConnection.java
@@ -64,10 +64,6 @@ public final class RemoteConnection {
                 RemoteConnection connection,
                 DisconnectCause disconnectCause) {}
 
-        /** @hide */
-        public void setDisconnectedWithSsNotification(RemoteConnection connection,
-                int disconnectCause, String disconnectMessage, int type, int code) {}
-
         /**
          * Invoked when this {@code RemoteConnection} is requesting ringback. See
          * {@link #isRingbackRequested()}.
@@ -102,6 +98,7 @@ public final class RemoteConnection {
          * @hide
          */
         public void onCallPropertiesChanged(RemoteConnection connection, int callProperties) {}
+
         /**
          * Invoked when the post-dial sequence in the outgoing {@code Connection} has reached a
          * pause character. This causes the post-dial signals to stop pending user confirmation. An
@@ -555,7 +552,7 @@ public final class RemoteConnection {
     /**
      * @return A bitmask of the properties of the {@code RemoteConnection}, as defined in
      *         {@link CallProperties}.
-     * @hide
+     *  @hide
      */
     public int getCallProperties() {
         return mCallProperties;
@@ -876,15 +873,6 @@ public final class RemoteConnection {
             for (Callback c : mCallbacks) {
                 c.onDisconnected(this, mDisconnectCause);
             }
-        }
-    }
-
-    /** @hide */
-   public void setDisconnectedWithSsNotification(int disconnectCause,
-                String disconnectMessage, int type, int code) {
-        for (Callback c : mCallbacks) {
-            c.setDisconnectedWithSsNotification(this, disconnectCause,
-                    disconnectMessage, type, code);
         }
     }
 
