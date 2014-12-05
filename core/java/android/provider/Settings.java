7b98258784e5a2dba954adf49634170a1e5a4384
Lars Greiss <kufikugel@googlemail.com>
2014-12-05 21:35:55 +0100

    Frameworks: Wifi and carrier label visibility control
    Port from kitkat
    
    ability to show either carrier, wifi both or neither of this labels.
    
    Change-Id: I0a951e8c620e8b0083470e0c0d3cd99a27589661

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2108,6 +2108,12 @@ public final class Settings {
         public static final String STATUS_BAR_TICKER_ENABLED = "status_bar_ticker_enabled";
 
         /**
+         * Hide network labels in the notification drawer
+         * @hide
+         */
+        public static final String NOTIFICATION_HIDE_LABELS = "notification_hide_labels";
+
+        /**
          * Control whether the process CPU usage meter should be shown.
          *
          * @deprecated Use {@link Global#SHOW_PROCESSES} instead
--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -52,6 +52,7 @@ import com.android.internal.telephony.cdma.EriInfo;
 import com.android.internal.util.AsyncChannel;
 import com.android.systemui.DemoMode;
 import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.phone.StatusBarHeaderView;
 
 import java.io.FileDescriptor;
@@ -127,6 +128,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
     int mQSWifiIconId = 0;
     int mWifiActivityIconId = 0; // overlay arrows for wifi direction
     int mWifiActivity = WifiManager.DATA_ACTIVITY_NONE;
+
+    int mHideLabels = PhoneStatusBar.LABELS_SHOW_ALL;
 
     // bluetooth
     protected boolean mBluetoothTethered = false;
@@ -1472,6 +1475,17 @@ public class NetworkControllerImpl extends BroadcastReceiver
                     + "/" + getResourceName(mBluetoothTetherIconId));
         }
 
+        switch (mHideLabels) {
+            case PhoneStatusBar.LABELS_HIDE_CARRIER:
+                mobileLabel = "";
+                combinedLabel = wifiLabel;
+                break;
+            case PhoneStatusBar.LABELS_HIDE_WIFI:
+                wifiLabel = "";
+                combinedLabel = mobileLabel;
+            break;
+        }
+
         // update QS
         for (NetworkSignalChangedCallback cb : mSignalsChangedCallbacks) {
             notifySignalsChangedCallbacks(cb);
@@ -1687,6 +1701,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
             return SignalStrength.SIGNAL_STRENGTH_NONE_OR_UNKNOWN;
         }
         return mSignalStrength.getGsmLevel();
+    }
+
+    public void setHideLablesMode(int mode) {
+        mHideLabels = mode;
     }
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
