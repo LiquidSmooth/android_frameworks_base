762dd27ea20b55f234d7ff75f5a35f8f7afa8522
Steve Kondik <shade@chemlab.org>
2015-02-01 16:51:03 -0500

    Stylus gestures features (1/2)
      Forward-port to CM-12.0
    
    Patchset: 3
    http://review.cyanogenmod.org/#/c/87654/
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/cm_symbols.xml
    	core/res/res/values/config.xml
    
    Change-Id: I230578a79071f4973299ecc1126987685b26d49b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -258,7 +258,6 @@ public final class Settings {
      * Input: Nothing.
      * <p>
      * Output: Nothing.
-
      */
     @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
     public static final String ACTION_WIFI_SETTINGS =
@@ -3143,6 +3142,55 @@ public final class Settings {
          * @hide
          */
         public static final String LOCK_SCREEN_ICON_COLOR = "lock_screen_icon_color";
+
+        /**
+         * Enable Stylus Gestures
+         *
+         * @hide
+         */
+        public static final String ENABLE_STYLUS_GESTURES = "enable_stylus_gestures";
+
+        /**
+         * Left Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_LEFT_SWIPE = "gestures_left_swipe";
+
+        /**
+         * Right Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_RIGHT_SWIPE = "gestures_right_swipe";
+
+        /**
+         * Up Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_UP_SWIPE = "gestures_up_swipe";
+
+        /**
+         * down Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_DOWN_SWIPE = "gestures_down_swipe";
+
+        /**
+         * Long press Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_LONG_PRESS = "gestures_long_press";
+
+        /**
+         * double tap Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_DOUBLE_TAP = "gestures_double_tap";
 
         /**
          * Whether lock-to-app will be triggered by long-press on recents.
