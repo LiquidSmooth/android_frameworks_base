8fcdd4693f8904c2681d316338b89bfe8fff66de
CPA-Poke <brock.shahan@cox.net>
2015-02-03 16:53:01 -0500

    Remove custom nav bar bits from PieTrigger
    * Also fix navbar derp
    * Fix another derp
    
    Change-Id: I363fdea6a12c71e0bf3aea0aef563f5d333d6e0d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4693,7 +4693,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     // If we aren't pressing recents right now then they presses
                     // won't be together, so send the standard long-press action.
                     sendBackLongPress = true;
-                } else if (NavbarEditor.NAVBAR_RECENT.equals(v.getTag()) {
+                } else if (NavbarEditor.NAVBAR_RECENT.equals(v.getTag())) {
                     hijackRecentsLongPress = true;
                 }
                 mLastLockToAppLongPress = time;
@@ -4701,7 +4701,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 // If this is back still need to handle sending the long-press event.
                 if (NavbarEditor.NAVBAR_BACK.equals(v.getTag())) {
                     sendBackLongPress = true;
-                } else if (NavbarEditor.NAVBAR_RECENT.equals(v.getTag()) {
+                } else if (NavbarEditor.NAVBAR_RECENT.equals(v.getTag())) {
                     hijackRecentsLongPress = true;
                 } else if (isAccessiblityEnabled && activityManager.isInLockTaskMode()) {
                     // When in accessibility mode a long press that is recents (not back)
