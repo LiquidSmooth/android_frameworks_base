3e6b90e577e9573bab9f3f4ee07c5bdd84864abf
Sajito <sajito@web.de>
2015-02-15 13:17:12 +0100

    Revert "remove double vibrate"
    This reverts commit 7b0ef7849afae23a8f26866a8f3874f30124fbe5.
    
    Change-Id: Ica107c1c7b3572b46df96418120bf0e8ea4e2500

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5331,7 +5331,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (useHapticFeedback) {
-            //performHapticFeedbackLw(null, HapticFeedbackConstants.VIRTUAL_KEY, false);
+            performHapticFeedbackLw(null, HapticFeedbackConstants.VIRTUAL_KEY, false);
         }
 
         if (isWakeKey) {
