dbc1df1b3ae31f7e11809869386d6383001e1d7e
Steve Kondik <steve@cyngn.com>
2015-02-18 03:52:24 -0800

    livedisplay: Implement active display adjustment
     * LiveDisplay is our new name for the various display technologies
       which adjust the screen based on environmental conditions other
       than the standard automatic backlight controls.
     * This patch implements automatic color temperature adjustment based
       on time of day. This is similar to f.lux or Redshift. My eyes are
       so happy now!
     * Automatic outdoor/SRE feature is now handled here.
     * Handling of CABC/CABL and color enhancements also handled here.
     * Manual RGB tweaking is handled here.
     * Can delegate to DisplayColor HAL if available, otherwise uses
       SurfaceFlinger's 1015 operation to apply changes.
     * Happily coexists with the new accessibility features for color
       blindness correction and color inversion.
     * All postprocessing will be disabled when powersave mode is
       activated.
    
    Change-Id: Iabd54d81ae6c6865ae2b56d2d405c9ed8ebcd2e2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.NfcTile;
@@ -342,6 +343,8 @@ public class QSTileHost implements QSTile.Host {
                 return new ExpandedDesktopTile(this);
             case QSConstants.TILE_REBOOT:
                 return new RebootTile(this);
+            case QSConstants.TILE_LIVE_DISPLAY:
+                return new LiveDisplayTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
