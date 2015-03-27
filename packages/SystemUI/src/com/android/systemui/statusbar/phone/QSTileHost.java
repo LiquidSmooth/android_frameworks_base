0e4ce78673a013e1b2386d39940dd8a0a7a1e21c
CPA-Poke <brock.shahan@cox.net>
2015-03-27 13:40:11 -0500

    Revert "Remove Notifications tile"
    This reverts commit ee2af2012a295d03b47844f897f94f37c0a23d8a.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -283,6 +283,8 @@ public class QSTileHost implements QSTile.Host {
                 return new CastTile(this);
             case QSConstants.TILE_HOTSPOT:
                 return new HotspotTile(this);
+            case QSConstants.TILE_NOTIFICATIONS:
+                return new NotificationsTile(this);
             case QSConstants.TILE_DATA:
                 return new DataTile(this);
             case QSConstants.TILE_ROAMING:
