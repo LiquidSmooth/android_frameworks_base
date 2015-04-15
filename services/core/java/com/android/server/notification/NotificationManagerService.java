73b87430e74fae7cc19f614de476a630ed48d9c4
Roman Birg <roman@cyngn.com>
2015-04-15 15:38:50 -0700

    Return of Silent mode (1/2)
    - Adds ability to replace "None" with "Silent" in the Volume panel
    
    - Right now, it will behave exactly the same as "None",
    except allow alarms to go through
    
    Change-Id: Icee44a7114e9529d139c52e5e37e3ec747cec336
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1015,6 +1015,7 @@ public class NotificationManagerService extends SystemService {
             mDisableNotificationEffects = true;
         }
         mZenModeHelper.readZenModeFromSetting();
+        mZenModeHelper.readSilentModeFromSetting();
         mInterruptionFilter = mZenModeHelper.getZenModeListenerInterruptionFilter();
 
         mUserProfiles.updateCache(getContext());
@@ -1704,7 +1705,8 @@ public class NotificationManagerService extends SystemService {
         if (mDisableNotificationEffects) {
             return "booleanState";
         }
-        if ((mListenerHints & HINT_HOST_DISABLE_EFFECTS) != 0) {
+        if ((mListenerHints & HINT_HOST_DISABLE_EFFECTS) != 0
+                && !mZenModeHelper.getIsNoneSilent()) {
             return "listenerHints";
         }
         if (mCallState != TelephonyManager.CALL_STATE_IDLE && !mZenModeHelper.isCall(record)) {
