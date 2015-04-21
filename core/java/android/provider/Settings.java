f0580e42bc9a0ed03ebae5173e813dc86885ed53
deadman96385 <seanhoyt963@gmail.com>
2015-04-20 23:44:33 -0400

    [1/3] frameworks/base: Add NFC polling setting
    Change-Id: I29b4a49d846da3b8eebf6c92cc3745f035034603
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3608,6 +3608,11 @@ public final class Settings {
         public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
 
         /**
+         * Define when Nfc polling is activated.
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
