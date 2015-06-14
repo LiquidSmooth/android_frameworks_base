AICP/frameworks_base

QS: Squashed from EuphoriaOS (1/2)
Change-Id: I69b2932f71f9d0d5797a80397563fef9f9297148

packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -100,6 +100,11 @@ public class LocationTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        mHost.startSettingsActivity(LOCATION_SETTINGS);
+    }
+
+    @Override
     protected void handleLongClick() {
         mHost.startSettingsActivity(LOCATION_SETTINGS_INTENT);
     }

LiquidSmooth/android_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -104,6 +104,11 @@ public class LocationTile extends QSTile<QSTile.BooleanState> {
     }
 
     @Override
+    protected void handleSecondaryClick() {
+        mHost.startSettingsActivity(LOCATION_SETTINGS_INTENT);
+    }
+
+    @Override
     protected void handleLongClick() {
         mHost.startSettingsActivity(LOCATION_SETTINGS_INTENT);
     }